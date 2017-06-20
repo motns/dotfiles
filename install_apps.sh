239d5a77c897e75b8222d7c46965a9e270be84b4
Adam Borocz <boroczadam@gmail.com>
2017-06-20 12:01:44 +0100

    Add Brew/Cask commands for installing required applications

---
+++ install_apps.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Essentials
+brew install fswatch
+brew install wget
+brew install htop
+brew cask install istat-menus
+brew cask install google-chrome
+
+# Base Dev tools
+brew install nvm
+brew install docker
+brew install terraform
+brew install packer
+brew cask install paw
+brew cask install github-desktop
+
+# Go development
+brew install go --with-cc-common
+brew install glide
+
+# Scala development
+brew cask install java
+brew install sbt
+brew install scala
+
+# DB tools
+brew cask install postico
+brew cask install pgadmin4
+brew cask install sequel-pro
+
+# Local dev server
+brew cask install virtualbox
+brew cask install vagrant
+
+# IDEs and code editors
+brew cask install sublime-text
+brew cask install atom
+brew cask install intellij-idea
+
+# Misc.
+brew cask install dropbox
+brew cask install spotify
+brew cask install viscosity
