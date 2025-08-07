e788d06a401fcc7ae26f411df5c33b46c6fee863
Adam Borocz <github@motns.io>
2025-08-07 14:42:34 +0100

    Add bin path for packages installed for user only

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,6 @@
 export PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/sbin
 export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/opt/local/bin
 export PATH=$PATH:/opt/homebrew/bin
 
